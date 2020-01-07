7e019efb25aaba7ef080c41ef624286010a6dfb5
Amy Franz <amy.franz@gmail.com>
2020-01-07 10:11:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
     new_item = find_item_by_name_in_collection(coupons[i][:item],cart)
     coup_item_name= "#{coupons[i][:item]} W/COUPON"
     cart_item_w_coupon = find_item_by_name_in_collection(coup_item_name, cart)
-    if new_item && new_item[:count] > cart_item_w_coupon[i][:num]
+    if new_item && new_item[:count] >= coupons[i][:num]
       if cart_item_w_coupon
         cart_item_w_coupon[:count] += cart_item_w_coupon[i][:num]
         new_item[:count] -= cart_item_w_coupon[i][:num]
